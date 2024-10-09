eda23beb68d81e6407b5bb566b055e0c30418d2a
baazz <yaoeloge@outlook.fr>
2024-10-09 17:42:59 +0000

    refactor(review) : mise a jour de la logique metier

--- app/Models/Mention.php
+++ app/Models/Mention.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class mention extends Model
+class Mention extends Model
 {
     use HasFactory;
     protected $fillable = [
